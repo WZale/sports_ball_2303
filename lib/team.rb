9eb3814f8dbbe0bc75630e3d4c5311b69a06f43b
William Zale <williamzale@gmail.com>
2023-03-29 12:43:55 -0400

    Add player_count method

--- lib/team.rb
+++ lib/team.rb
@@ -1,7 +1,14 @@
 class Team
-  attr_reader :name, :location
+  attr_reader :name, 
+              :location,
+              :roster
   def initialize(name, location)
     @name = name
     @location = location
+    @roster = []
+  end
+
+  def player_count
+    @roster.length
   end
 end
