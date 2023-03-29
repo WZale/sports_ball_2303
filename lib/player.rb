aca4656147fce492f101fecd16f5e2bca2cda06e
William Zale <williamzale@gmail.com>
2023-03-29 11:51:04 -0400

    Fix montly_cost typo

--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,7 @@ class Player
   attr_reader :name, :monthly_cost, :contract_length
   def initialize(name, monthly_cost, contract_length)
     @name = name
-    @montly_cost = monthly_cost
+    @monthly_cost = monthly_cost
     @contract_length = contract_length
   end
 
