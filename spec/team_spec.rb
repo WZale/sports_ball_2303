77e01a491ebca85bf16a6573a98ea8675c05a03d
William Zale <williamzale@gmail.com>
2023-03-29 12:54:00 -0400

    Add test for add player method

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -4,8 +4,27 @@ require './lib/team'
 
 RSpec.describe Team do
   it 'exists' do
-    player = team = Team.new("Dodgers", "Los Angeles")
+    team = Team.new("Dodgers", "Los Angeles")
 
     expect(team).to be_a Team
+    expect(team.roster).to eq([])
+    expect(team.player_count).to eq(0)
+
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    expect(player_1).to be_a Player
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    expect(player_2).to be_a Player
+  end
+
+  it 'has an add player method' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    player_2 = Player.new("Kenny DeNunez", 500000, 24)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+
+    expect(team.roster).to eq([player_1, player_2])
+    expect(team.player_count).to eq(2)
   end
 end
