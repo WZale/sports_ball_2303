dafa033d6997dab921c1c8b4152c9fd7f4659d3b
William Zale <williamzale@gmail.com>
2023-03-29 11:54:01 -0400

    Add total_cost method

--- lib/player.rb
+++ lib/player.rb
@@ -8,11 +8,14 @@ class Player
 
 
   def first_name
-   name.split[0]
+    name.split[0]
   end
 
   def last_name
     name.split[1]
-   end
+  end
 
+  def total_cost
+    contract_length * monthly_cost
+  end
 end
