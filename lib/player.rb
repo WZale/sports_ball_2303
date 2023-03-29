c160457de25045dd68fab69496e6565d2e37c289
William Zale <williamzale@gmail.com>
2023-03-29 11:46:12 -0400

    Add first_name method

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,15 @@
 class Player
+  attr_reader :name, :monthly_cost, :contract_length
   def initialize(name, monthly_cost, contract_length)
-
+    @name = name
+    @montly_cost = monthly_cost
+    @contract_length = contract_length
   end
+
+
+  def first_name
+   name.split[0]
+  end
+
+  
 end
